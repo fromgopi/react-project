9226fffcf140c1da40e80df02a2044c0492d1bed
Gopi Krishna Muktevi <muktevigk@handbookinc.com>
2018-06-03 19:37:19 +0530

    Some changes in Courses file

--- src/components/jobs/AllCourses.js
+++ src/components/jobs/AllCourses.js
@@ -1,5 +1,11 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
 import Table from '@material-ui/core/Table'
+import TableCell from '@material-ui/core/TableCell'
+import TableRow from '@material-ui/core/TableRow'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles'
 
 class AllCourses extends Component{
 
@@ -28,17 +34,43 @@ class AllCourses extends Component{
 
     render(){
         return(
-            <div>
-                <h1>Courses</h1>
-                <div>
-                    {this.state.courses.map(function (course, index) {
-                        return(<h3 keys={index}>{course.title}</h3>)
-                    })}
-                </div>
-            </div>
+            <Fragment>
+                <h2>Courses</h2>
+                <Table className={this.props.classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Title</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.state.courses.map(function (course, index) {
+                            return(
+                                <TableRow key={index}>
+                                    <TableCell component="th" scope="row">{course.title}</TableCell>
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+            </Fragment>
         );
     }
-
 }
 
-export default AllCourses;
+const styles = theme => ({
+    root: {
+        width: '100%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 700,
+    },
+});
+
+AllCourses.propsTyps = {
+    classes: PropTypes.object.isRequired,
+};
+
+
+export default withStyles(styles)(AllCourses);
